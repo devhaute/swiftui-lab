af9f90beaeb5b7ca0702cb0e747299e7579f426f
devhaute <devhaute@gmail.com>
2021-09-30 23:53:13 +0900

    CustomTabView 환경 세팅

---
+++ swiftui-lab/CustomTabView/index.swift
@@ -0,0 +1,14 @@
+//
+//  index.swift
+//  swiftui-lab
+//
+//  Created by devhaute on 2021/09/30.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text("CustomTabView")
+    }
+}
---
+++ swiftui-lab/swiftui_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_labApp.swift
+//  swiftui-lab
+//
+//  Created by devhaute on 2021/09/30.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
