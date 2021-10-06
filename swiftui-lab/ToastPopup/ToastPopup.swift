a044914c65d26172a9741c4773cba4cda9b42b19
devhaute <devhaute@gmail.com>
2021-10-06 13:54:16 +0900

    BottomToastMessage 구현

--- swiftui-lab/ToastPopup/ToastPopup.swift
+++ swiftui-lab/ToastPopup/ToastPopup.swift
@@ -15,7 +15,7 @@ struct ToastPopupView: View {
                 Text("안내 메세지")
                     .fontWeight(.black)
                     .foregroundColor(.white)
-
+                
                 Text("토스트 메세지 입니다!")
                     .lineLimit(2)
                     .font(.system(size: 14))
@@ -28,6 +28,31 @@ struct ToastPopupView: View {
         .frame(width: UIScreen.main.bounds.width)
         .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom == 0 ? 0 : 15)
         .background(Color.purple)
+    }
+    
+    func createBottomToastMessage() -> some View {
+        HStack(alignment: .top, spacing: 10) {
+            Image("cat")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .scaledToFill()
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading) {
+                Text("내 고양이")
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                
+                Text("토스트 메세지 입니다!")
+                    .font(.system(size: 14))
+                    .foregroundColor(.white)
+                Divider().opacity(0)
+            }
+        }
+        .frame(width: 300)
+        .padding(15)
+        .background(Color.green)
+        .cornerRadius(20)
     }
     
     var body: some View {
@@ -56,12 +81,11 @@ struct ToastPopupView: View {
                         .background(Color.green)
                         .cornerRadius(10)
                 }
-                
             }
         }
         .edgesIgnoringSafeArea(.all)
-        .popup(isPresented: self.$shouldShowBottomSolidMessage, type: .toast, position: .bottom, animation: .easeInOut, autohideIn: 2, closeOnTap: true, closeOnTapOutside: true) {
-            self.createBottomSolidMessage()
+        .popup(isPresented: $shouldShowBottomToastMessage, type: .floater(verticalPadding: 20), position: .bottom, animation: .spring(), autohideIn: 2, closeOnTap: true, closeOnTapOutside: true) {
+            self.createBottomToastMessage()
         }
     }
 }
